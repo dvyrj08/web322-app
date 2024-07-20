98af28c8619570529ff7868569b7196a47e563dc
dvyrj08 <you@example.com>
2024-07-19 23:58:27 -0400

    d

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const path = require('path');
 const storeService = require('./store-service');
 
 const app = express();
-const PORT = process.env.web322-app-nine.vercel.app/about;
+const PORT = process.env.PORT || 8080;
 
 // Cloudinary configuration
 cloudinary.config({
